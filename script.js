db8248024082bf0178d05f4e4e213c4985ca4a20
RishankD <156447323+RishankD@users.noreply.github.com>
2024-11-12 23:49:33 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Constructor function for Ferrari F70 Aperta Toy Car
+function ToyCar(model, color, material, scale, brand, price, doors, dimensions, weight, wheels, features, packaging, releaseYear, ageRecommendation) {
+    this.model = model;
+    this.color = color;
+    this.material = material;
+    this.scale = scale;
+    this.brand = brand;
+    this.price = price;
+    this.doors = doors;
+    this.dimensions = dimensions;
+    this.weight = weight;
+    this.wheels = wheels;
+    this.features = features;
+    this.packaging = packaging;
+    this.releaseYear = releaseYear;
+    this.ageRecommendation = ageRecommendation;
+  }
+  
