e0795e7f43710f974a5358f2f569a6dbd8bc4370
RishankD <156447323+RishankD@users.noreply.github.com>
2024-11-12 23:49:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function ToyCar(model, color, material, scale, brand, price, doors, dimensions, 
     this.ageRecommendation = ageRecommendation;
   }
   
+ // Create an instance for the Ferrari F70 Aperta Toy Car
+  const ferrariF70 = new ToyCar(
+    "Ferrari F70 Aperta",
+    "Black",
+    "Diecast",
+    "1:24",
+    "BBurago",
+    17.99,
+    2,
+    { length: "7.5 inches", width: "3.5 inches", height: "2.5 inches" },
+    0.4,
+    4,
+    ["Movable parts", "Openable doors"],
+    "Standard cardboard box",
+    2023,
+    "8+"
+  );
+  
